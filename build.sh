c1276ba9365ba0e59afb63813c4e52b67a67c389
SuperCatss <24800663+SuperCatss@users.noreply.github.com>
2024-02-04 13:12:33 +0800

    add build sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 输出文件路径
+output_file="CN.rsc"
+
+# 输出文件的开头内容
+header="/log info \"Loading CN ipv4 address list\"\n/ip firewall address-list remove [/ip firewall address-list find list=CN]\n/ip firewall address-list"
+
+# 创建输出文件并写入开头内容
+echo -e $header > $output_file
+
+# 使用curl下载文件并保存为输入文件
+curl -JLO https://github.com/gaoyifan/china-operator-ip/blob/ip-lists/china.txt
+# 逐行读取输入文件
+while IFS= read -r line
+do
+  # 将每行的CIDR IP地址添加到输出文件中
+  echo ":do { add address=$line list=CN } on-error={}" >> $output_file
+done < "china.txt"
+
+echo "生成的文件已保存为$output_file"
